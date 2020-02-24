2053a3d23c4003efd28f4c37d9f5c2c5e69882a7
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-24 16:37:30 -0700

    users CRUD complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+roles = ['patient', 'doctor']
+10.times do
+  doctor = Doctor.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name
+  )
+ 
+  2.times do 
+    user = User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name
+    )
+
+    Appointment.create(
+      role: roles.sample,
+      doctor_id: doctor.id,
+      user_id: user.id,
+      date: Faker::Time.forward(days: 31,  period: :morning, format: :long)
+    )
+  end
+end
+
+puts "Data Seeded."
