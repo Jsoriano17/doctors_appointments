98a153cf4f23e061f8a5df7ad80562f1890543d6
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-24 15:29:46 -0700

    added controllers for doctor and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'doctors/index'
+  get 'doctors/show'
+  get 'doctors/new'
+  get 'doctors/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
